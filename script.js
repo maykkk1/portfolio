ebaf94c28649a0379d993ec2d6e4eeba74ac47d8
maykkk1 <maycon5591@gmail.com>
2022-01-26 18:38:39 -0300

    secao projetos atualizada

--- script.js
+++ script.js
@@ -6,35 +6,6 @@ function toggleMenu() {
     nav.classList.toggle('active-nav')
 }
 
-let i = 0; 
 
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img")
-
-const desc = document.getElementById("descricao")
-const descItems = document.querySelectorAll("#descricao div")
-
-btnNext = document.querySelector('#next-btn')
-btnPrevious = document.querySelector('#previous-btn')
-btnNext.addEventListener('click', next);
-btnPrevious.addEventListener('click', previous)
-
-function next() {
-    i++
-    if(i > img.length - 1) {
-        i = 0;
-    }
-    imgs.style.transform = `translateX(${-i * 796.6}px)`;
-    desc.style.transform = `translateX(${-i * 341.4}px)`;
-}
-
-function previous() {
-    i--
-    if(i < 0) {
-        i = img.length - 1;
-    }
-    imgs.style.transform = `translateX(${-i * 796.6}px)`;
-    desc.style.transform = `translateX(${-i * 341.4}px)`;
-}
 
 
